26996c2fd95daa944c3037e2472b7114385fe537
deanban <pbp2005@gmail.com>
2019-07-08 19:24:54 -0400

    theme mode working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import List from './components/List';
 import ActionButton from './components/ActionButton';
 import { sortCardOrList } from './actions';
+import Appbar from './components/Appbar';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: ${props =>
+      props.theme.mode === 'dark' ? '#203A43' : '#eee'};
+    color: ${props => (props.theme.mode === 'dark' ? '#EEE' : '#203A43')};
+
+  }
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
+  position: absolute;
+  top: 7vh;
+  color: #2c3e50;
 `;
 
 class App extends Component {
+  state = { mode: 'dark' };
+
   onDragEnd = result => {
     // console.log(this);
     //reorder after drag
@@ -33,27 +48,39 @@ class App extends Component {
   render() {
     const { lists } = this.props;
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <div>
-          <h2>Welcome to Bootlerello.</h2>
-          <Droppable droppableId="list" direction="horizontal" type="list">
-            {provided => (
-              <Container {...provided.droppableProps} ref={provided.innerRef}>
-                {lists.map((list, index) => (
-                  <List
-                    key={list.id}
-                    listId={list.id}
-                    title={list.title}
-                    cards={list.cards}
-                    index={index}
-                  />
-                ))}
-                <ActionButton list />
-              </Container>
-            )}
-          </Droppable>
-        </div>
-      </DragDropContext>
+      <ThemeProvider theme={this.state}>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <GlobalStyle />
+          <Appbar
+            theme={() =>
+              this.setState(({ mode }) =>
+                mode === 'light'
+                  ? this.setState({ mode: 'dark' })
+                  : this.setState({ mode: 'light' })
+              )
+            }
+          />
+          {/* {console.log(this.state)} */}
+          <div>
+            <Droppable droppableId="list" direction="horizontal" type="list">
+              {provided => (
+                <Container {...provided.droppableProps} ref={provided.innerRef}>
+                  {lists.map((list, index) => (
+                    <List
+                      key={list.id}
+                      listId={list.id}
+                      title={list.title}
+                      cards={list.cards}
+                      index={index}
+                    />
+                  ))}
+                  <ActionButton list />
+                </Container>
+              )}
+            </Droppable>
+          </div>
+        </DragDropContext>
+      </ThemeProvider>
     );
   }
 }
